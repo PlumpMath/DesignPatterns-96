767392209373e9e64c20dfa0263d2f0a0c37af08
fatihkck <fatihkocak1@gmail.com>
2017-02-19 19:48:01 +0300

    Decorator

---
+++ Structural/Decorator/CrmService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Decorator
+{
+
+    public interface ICrmService
+    {
+        void Save(int id);
+    }
+
+    public class CrmService : ICrmService
+    {
+        public void Save(int id)
+        {
+            Console.WriteLine("Crm services working");
+        }
+    }
+}
--- Structural/Decorator/Program.cs
+++ Structural/Decorator/Program.cs
@@ -1,15 +1,95 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Decorator
 {
+    #region Participants
+    //if you want to extend the functionality of objects, you can use this pattern.
+    //Component : defines the interface for objects that can have responsibilities added to them dynamically.
+    //ConcreteComponent : defines an object to which additional responsibilities can be attached.
+    //Decorator : maintains a reference to a Component object and defines an interface that conforms to Component's interface.
+    //ConcreteDecorator : adds responsibilities to the component.
+    #endregion
+
+
+    /// <summary>
+    /// Component
+    /// </summary>
+    public interface ICustomerService
+    {
+        bool CreateUser(string name);
+    }
+
+    /// <summary>
+    /// ConcreteComponent
+    /// </summary>
+    public class CustomerServiceDecorator : ICustomerService
+    {
+        protected readonly ICustomerService _customerService;
+
+        public CustomerServiceDecorator(
+            ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        public virtual bool CreateUser(string name)
+        {
+            return _customerService.CreateUser(name);
+        }
+    }
+
+
+    /// <summary>
+    /// Decorator
+    /// </summary>
+    public class CustomerService : ICustomerService
+    {
+        public bool CreateUser(string name)
+        {
+            Console.WriteLine("Create user process");
+            return true;
+        }
+    }
+
+
+    /// <summary>
+    /// ConcreteDecorator
+    /// </summary>
+    public class CustomerWithCRMService : CustomerServiceDecorator
+    {
+
+        private readonly ICrmService _crmServices;
+        public CustomerWithCRMService(
+            ICustomerService customerService,
+            ICrmService crmService) : base(customerService)
+        {
+            //crm servis call
+            _crmServices = crmService;
+        }
+
+        public override bool CreateUser(string name)
+        {
+            Console.WriteLine("Crm log process");
+            _crmServices.Save(1);
+            return base.CreateUser(name);
+        }
+
+
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
+
+            //you can use ioc (castle windsor, autofac decorator patternt)
+            ICustomerService _customerService = new CustomerService();
+            ICustomerService _customerWithCrm = new CustomerWithCRMService(_customerService, new CrmService());
+            _customerWithCrm.CreateUser("fiti fiti");
+
+            Console.Read();
+
         }
     }
 }
