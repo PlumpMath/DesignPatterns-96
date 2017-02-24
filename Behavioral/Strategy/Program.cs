69b3e53715bb545fbc56d431b4239ce2a8c88eb5
fatihkck <fatihkocak1@gmail.com>
2017-02-24 17:27:31 +0300

    Pattern

---
+++ Behavioral/Strategy/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+
+    #region Participants
+    //Define a family of algorithms, encapsulate each one, and make them interchangeable.
+    //Strategy
+    //ConcreteStrategy 
+    //Context
+    #endregion  
+
+    public interface IShippingCost
+    {
+        decimal Calculate();
+    }
+
+    public class UpsShippingCost : IShippingCost
+    {
+        public decimal Calculate()
+        {
+            return 3.1m;
+        }
+    }
+
+    public class FedexShippingCost : IShippingCost
+    {
+        public decimal Calculate()
+        {
+            return 5.1m;
+        }
+    }
+
+
+    public class ShippingCostContext : IShippingCost
+    {
+        private readonly IShippingCost _shippingCost;
+        public ShippingCostContext(IShippingCost shippingCost)
+        {
+            _shippingCost = shippingCost;
+        }
+
+        public decimal Calculate()
+        {
+            return _shippingCost.Calculate();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var shippingCost = new ShippingCostContext(new FedexShippingCost());
+            Console.WriteLine(shippingCost.Calculate());
+
+            shippingCost = new ShippingCostContext(new UpsShippingCost());
+            Console.WriteLine(shippingCost.Calculate());
+
+            Console.Read();
+        }
+    }
+}
